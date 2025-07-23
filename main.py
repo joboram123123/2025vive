fee1da1df0eb7395543ca166323cf9ca98a98461
joboram123123 <boraup61@gmail.com>
2025-07-23 13:33:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 import streamlit as st
 import time
 
+# 세션 상태 초기화
+if "count" not in st.session_state:
+    st.session_state.count = 5
+if "running" not in st.session_state:
+    st.session_state.running = False
+
 y = st.empty()
 y.write('please Click start button')
 
-c1, c2, c3,_=st.columns([1,1,1,5])
-start = c1.button('시작', key=1)
-clear = c2.button('클리어', key=2)
-c3.button('리셋', key = 3)
+# 버튼 배치
+c1, c2, c3, _ = st.columns([1, 1, 1, 5])
+start = c1.button('시작', key='start')
+clear = c2.button('클리어', key='clear')
+reset = c3.button('리셋', key='reset')
 
+# 버튼 동작 정의
 if start:
+    st.session_state.running = True
+
+if clear:
+    st.session_state.count = 0
+    st.session_state.running = False
+
+if reset:
+    st.session_state.count = 5
+    st.session_state.running = False
+
+# 타이머 동작
+if st.session_state.running and st.session_state.count > 0:
     with y:
-        for i in range(6):
-            t = 5-i
-            st.write(f'카운트다운 {t} 초')
-            time.sleep(1)
+        st.write(f'카운트다운 {st.session_state.count} 초')
+    time.sleep(1)
+    st.session_state.count -= 1
+    st.experimental_rerun()
+elif st.session_state.count == 0 and st.session_state.running:
+    with y:
+        st.success("⏰ 타이머 종료!")
+    st.session_state.running = False
