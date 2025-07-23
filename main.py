b7a35f2bb670cf72c6883ee5a21088cadaede843
joboram123123 <boraup61@gmail.com>
2025-07-23 13:39:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,12 @@ y = st.empty()
 y.write('please Click start button')
 
 # 타이머 시작/정지 버튼
-def toggle_timer():
-    if not st.session_state.running:
-        st.session_state.start_time = time.time()
-        st.session_state.running = True
-    else:
-        st.session_state.running = False
 
-if st.button("▶️ 타이머 시작 / 일시정지", on_click=toggle_timer):
-    pass
+c1, c2, c3,_=st.columns([1,1,1,5])
+start = c1.button('시작', key=1)
+clear = c2.button('클리어', key=2)
+c3.button('리셋', key = 3)
+
 
 # 타이머 표시
 if st.session_state.running:
